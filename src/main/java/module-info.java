13f6b923c9924dd57798dd4c6bcbe979cf539426
Arthgar25 <desarthgar@gmail.com>
2026-01-18 11:35:00 -0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.version_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.version_1 to javafx.fxml;
+    exports com.example.version_1;
+}
