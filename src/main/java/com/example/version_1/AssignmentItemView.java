002948f93d519c7d6c895fa639ba924a0f7ad038
Arthgar25 <desarthgar@gmail.com>
2026-01-19 10:07:44 -0800

    Changed formatting in the information

--- src/main/java/com/example/version_1/AssignmentItemView.java
+++ src/main/java/com/example/version_1/AssignmentItemView.java
@@ -1,4 +1,5 @@
 package com.example.version_1;
+import javafx.geometry.Insets;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -8,14 +9,32 @@ public class AssignmentItemView extends HBox {
 
     public AssignmentItemView(Assignment assignment) {
         this.assignment = assignment;
+
+        // Spacing
+        setSpacing(12);
+        setPadding(new Insets(8,12,8,12));
+
+        // Rounded outline
+        setStyle("""
+            -fx-border-color: #cfcfcf;
+            -fx-border-radius: 8;
+            -fx-background-radius: 8;
+            -fx-background-color: #f9f9f9;
+        """);
+
         Label title = new Label(assignment.getTitle());
         Label due = new Label(assignment.getDueDate().toString());
         Label description = new Label(assignment.getDescription());
         Label assignmentType = new Label(assignment.getType().toString());
+
+        // Description styling
+        description.setStyle("-fx-text-fill: #666666;");
+
         CheckBox completedCheckBox = new CheckBox();
         completedCheckBox.selectedProperty().addListener((obs, oldVal, newVal) -> {
             assignment.setCompleted(newVal);
         });
-        getChildren().addAll(title,due, description,  assignmentType, completedCheckBox);
+
+        getChildren().addAll(completedCheckBox, title, due, assignmentType, description);
     }
 }
