12658099b6883e96b6d9841118b0d67b895bf9bd
Arthgar25 <desarthgar@gmail.com>
2026-01-18 19:29:10 -0800

    Completed the Assignment class with attributes, getters and setters

--- src/main/java/com/example/version_1/Assignment.java
+++ src/main/java/com/example/version_1/Assignment.java
@@ -5,7 +5,41 @@ import java.time.LocalDate;
 public class Assignment {
     private String title;
     private LocalDate dueDate;
-    private String type;
+    private AssignmentType type;
     private boolean completed;
+    private String description;
 
+    public Assignment(String title, LocalDate dueDate, AssignmentType type, String description) {
+        this.title = title;
+        this.title = title;
+        this.type = type;
+        this.dueDate = dueDate;
+        this.completed = false;
+        this.description = description;
+    }
+
+    //Getters
+    public String getTitle() {
+        return title;
+    }
+    public AssignmentType getType() {
+        return type;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    //Setters/Actions
+    public void markCompleted() {
+        this.completed = true;
+    }
 }
--- src/main/java/com/example/version_1/AssignmentType.java
+++ src/main/java/com/example/version_1/AssignmentType.java
@@ -2,11 +2,10 @@ package com.example.version_1;
 
 public enum AssignmentType {
     HOMEWORK,
-    PROJECT,
     PRESENTATION,
     WORKSHOP,
     EXTRA_HOUR,
-    QUIZ,
     TEST,
+    EVIDENCE,
     OTHER
 }
